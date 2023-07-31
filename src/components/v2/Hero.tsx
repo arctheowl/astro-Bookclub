1f31e454653393e7edff980e92afde23d90bd96a
lidevlin <lidevlin95@gmail.com>
2023-07-31 20:03:49 +0100

    Improved mobile hero section

--- src/components/v2/Hero.tsx
+++ src/components/v2/Hero.tsx
@@ -1,15 +1,17 @@
 import { Description } from "../../data/description";
-import Date from "../../data/nextMeeting.json"
+import Date from "../../data/nextMeeting.json";
 
 const Hero = () => {
   return (
     <div
       className={`bg-[url('/Images/HeroBackground.jpg')]
-        h-96 md:h-4/6 bg-opacity-10 text-white relative flex justify-center items-center`}
+        h-[70%] md:h-4/6 bg-opacity-10 text-white relative flex justify-center items-center`}
     >
       <div className="bg-black opacity-50 h-full w-full absolute"></div>
       <div className="flex flex-col text-center justify-center items-center absolute gap-2 font-serif">
-        <h1 className="text-4xl md:text-6xl text-center font-serif">Welcome to</h1>
+        <h1 className="text-4xl md:text-6xl text-center font-serif">
+          Welcome to
+        </h1>
         <h1 className="opacitiy-100 text-4xl md:text-6xl text-purple-300 font-serif">
           Fleet Book Club
         </h1>
@@ -19,7 +21,6 @@ const Hero = () => {
         <p className="text-xl md:text-2xl text-center font-serif text-gray-100">
           Our next meeting: {Date}
         </p>
-
       </div>
     </div>
   );
